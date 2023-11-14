e3f949161f65efb09b6a81e5de86f23cf97fa64e
NohaGhattas <147104656+NohaGhattas@users.noreply.github.com>
2023-11-14 18:28:43 +0200

    Add files via upload

---
+++ task02.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ok;
+ 
+/**
+ *
+ * @author nohaa
+ */
+public class Ok {
+          
+ public static void main(String[] args)throws Exception {
+for (int i = 0; i <= 100; i++) {
+  System.out.println(i);
+  if (i%3==0){
+  System.out.println("Fizz");
+}
+  if (i%5==0){
+      System.out.println("buzz");
+  }
+  if(i%3==0 && i%5==0){
+     System.out.println("fizz buzz"); 
+  }
+}
+}
+
+     }
+     
+
